35f663441069ddd6ba1b2fc5383c1d863e46ca19
dejwi <misti.krasula@gmail.com>
2022-05-19 00:55:39 +0200

    +animated

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { ReactComponent as FbLogo } from '../svg/fblogo.svg';
+import {motion} from "framer-motion";
+
+const animeOpts = {
+    initial:{ y: "-1.5rem" , opacity: 0},
+    animate:{ y: 0, opacity: 1},
+    transition:{ type: 'spring', delay: 0.2, duration: 2}
+};
 
 const Login: React.FC = () => {
     const apiUrl = process.env.REACT_APP_BACKEND as string;
 
-    return (<main className='h-screen flex flex-col justify-center items-center font-["Roboto"] gap-2.5'>
+    return (<motion.main {...animeOpts} className='h-screen flex flex-col justify-center items-center font-["Roboto"] gap-2.5'>
         <FbLogo className='absolute top-2.5'/>
+
         <a href={`${apiUrl}/auth/facebook`} className='bg-blue-500 w-48 text-center py-2 rounded text-white font-medium'>Sign-in with Facebook</a>
         <button className='bg-neutral-700 w-48 text-center py-2 rounded text-white font-medium'>Demo account</button>
-    </main>);
+    </motion.main>);
 };
 
 export default Login;
