8b81bbd5972ac01bc0bd61ed7779a524042f528c
dejwi <misti.krasula@gmail.com>
2022-05-21 19:01:06 +0200

    added bg property to user

--- api/models/user.js
+++ api/models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new Schema({
     username: { type: String, required: true },
     facebook_id: { type: String, required: true },
     picUrl: { type: String, required: true },
+    bgUrl: {type: String},
     friends: [ { type: Schema.Types.ObjectId, ref: 'User' } ],
     friendReqSend: [ { type: Schema.Types.ObjectId, ref: 'User' } ],
     friendReqReceived: [ { type: Schema.Types.ObjectId, ref: 'User' } ],
