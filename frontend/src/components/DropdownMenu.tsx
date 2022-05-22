011d3d2ce624cd915efd9bd7111e226397defede
dejwi <misti.krasula@gmail.com>
2022-05-23 01:10:04 +0200

    logout link

--- frontend/src/components/DropdownMenu.tsx
+++ frontend/src/components/DropdownMenu.tsx
@@ -13,10 +13,9 @@ const DropdownMenu: React.FC<props> = ({userid}) => {
   return (<motion.div className='absolute top-11 right-1 w-max grid bg-white shadow py-2 gap-2 text-left rounded'
               initial={{opacity: 0, y: -10, scale: 0.95}} animate={{opacity: 1, y: 0, scale: 1}} transition={{type: 'spring', duration: 0.4}} exit={{opacity: 0, y: -10, scale: 0.95}}>
     <Link to={`/user/${userid}`} className={linkClass}>Profile</Link>
-    <Link to={'/'} className={linkClass}>Friends</Link>
     <Link to={'/'} className={linkClass}>Settings</Link>
     <div className='h-px  bg-neutral-200 -mt-px -mb-px'></div>
-    <Link to={'/'} className={linkClass}>Logout</Link>
+    <Link to='/logout' className={linkClass}>Logout</Link>
   </motion.div>);
 };
 
