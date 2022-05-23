ae7d1ce64d9e6804b2ccbb4e4a0a350b1346f027
dejwi <misti.krasula@gmail.com>
2022-05-24 00:12:47 +0200

    google domain verify

--- api/app.js
+++ api/app.js
@@ -19,9 +19,10 @@ app.use(cors({
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug'  );
 
 // Init mongodb
 require('./config/mongodb');
