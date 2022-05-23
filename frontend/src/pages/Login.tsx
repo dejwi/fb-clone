cc99d063c16833c7eb93f7d4b8b3bebcc9913cf5
dejwi <misti.krasula@gmail.com>
2022-05-23 21:22:30 +0200

    added google auth option & new btns

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { ReactComponent as FbLogo } from '../svg/fblogo.svg';
 import {motion} from "framer-motion";
+import googleSignin from '../img/google2x.png';
+import facebookSignin from '../img/facebook.png';
+import FbLogoCustom from '../img/fakebooklogo-nobg.png';
 
 const animeOpts = {
     initial:{ y: -16 , opacity: 0},
@@ -11,10 +14,17 @@ const animeOpts = {
 const Login: React.FC = () => {
     const apiUrl = process.env.REACT_APP_BACKEND as string;
 
-    return (<motion.main {...animeOpts} className='h-screen flex flex-col justify-center items-center font-["Roboto"] gap-2.5'>
-        <FbLogo className='absolute top-2.5'/>
+    return (<motion.main {...animeOpts} className='h-screen flex flex-col justify-center items-center font-["Roboto"] gap-2.5 ' id='fb-root'>
+        {/*<FbLogo className='absolute top-2.5'/>*/}
+        <img alt='ds' src={FbLogoCustom} className='absolute top-2.5 w-12'/>
 
-        <motion.a whileHover={{scale: 1.05}} whileTap={{scale: 0.95}} href={`${apiUrl}/auth/facebook`} className='bg-blue-500 w-48 text-center py-2 rounded text-white font-medium'>Sign-in with Facebook</motion.a>
+        <h1 className='absolute bottom-3.5 text-xs text-zinc-600 font-[Poppins]'>Hi! Select either one to proceed</h1>
+        <motion.a whileHover={{scale: 1.05}} whileTap={{scale: 0.95}} href={`${apiUrl}/auth/facebook`} >
+            <img alt='signin facebook' src={facebookSignin} className='w-48 shadow-md'/>
+        </motion.a>
+        <motion.a whileHover={{scale: 1.05}} whileTap={{scale: 0.95}} href={`${apiUrl}/auth/google`} >
+            <img alt='signin google' src={googleSignin} className='w-[12.35rem]'/>
+        </motion.a>
         {/*<button className='bg-neutral-700 w-48 text-center py-2 rounded text-white font-medium'>Demo account</button>*/}
     </motion.main>);
 };
