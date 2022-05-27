496bcdfe227d8d6584b5e94b6eddf2e984c751ab
dejwi <misti.krasula@gmail.com>
2022-05-27 17:08:23 +0200

    added chat to pageswitch nav

--- frontend/src/components/Nav/PageSwitch.tsx
+++ frontend/src/components/Nav/PageSwitch.tsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { ReactComponent as Home } from '../../svg/home.svg'
 import { ReactComponent as People } from '../../svg/people.svg'
+import { ReactComponent as Chat } from '../../svg/chat.svg'
 
 import { ReactComponent as HomeOut } from '../../svg/homeOutline.svg'
 import { ReactComponent as PeopleOut } from '../../svg/peopleOutline.svg'
+import { ReactComponent as ChatOut } from '../../svg/chatOutline.svg'
+
 import {Link, useLocation} from 'react-router-dom';
 
 const PageSwitch: React.FC = () => {
-  const [current, setCurrent] = useState<'home' | 'friends' | 'none'>('home');
+  const [current, setCurrent] = useState<'home' | 'friends' | 'chat' | 'none'>('home');
   const loc = useLocation();
   const iconClass = 'w-7 h-7';
 
@@ -19,6 +22,9 @@ const PageSwitch: React.FC = () => {
       case '/friendsfeed':
         setCurrent('friends')
         break;
+      case '/chat':
+        setCurrent('chat')
+        break;
       default:
         setCurrent('none');
         break;
@@ -28,15 +34,27 @@ const PageSwitch: React.FC = () => {
   return (<div className='justify-self-center flex gap-2'>
     {current === 'home' && <>
       <Link to={'/'}><Home className={iconClass + ' fill-neutral-900 '}/></Link>
-      <Link to={'/friendsfeed'}><PeopleOut className={iconClass + ' fill-neutral-500'}/></Link> </>}
+      <Link to={'/friendsfeed'}><PeopleOut className={iconClass + ' fill-neutral-500'}/></Link>
+      <Link to={'/chat'}><ChatOut className={iconClass + ' fill-neutral-500'}/></Link>
+    </>}
 
     {current === 'friends' && <>
       <Link to={'/'}><HomeOut className={iconClass + ' fill-neutral-500'}/></Link>
-      <Link to={'/friendsfeed'}><People className={iconClass + ' fill-neutral-800'}/></Link> </>}
+      <Link to={'/friendsfeed'}><People className={iconClass + ' fill-neutral-800'}/></Link>
+      <Link to={'/chat'}><ChatOut className={iconClass + ' fill-neutral-500'}/></Link>
+    </>}
+
+    {current === 'chat' && <>
+      <Link to={'/'}><HomeOut className={iconClass + ' fill-neutral-500'}/></Link>
+      <Link to={'/friendsfeed'}><People className={iconClass + ' fill-neutral-500'}/></Link>
+      <Link to={'/chat'}><Chat className={iconClass + ' fill-neutral-800'}/></Link>
+    </>}
 
     {current === 'none' && <>
       <Link to={'/'}><HomeOut className={iconClass + ' fill-neutral-500'}/></Link>
-      <Link to={'/friendsfeed'}><PeopleOut className={iconClass + ' fill-neutral-500'}/></Link> </>}
+      <Link to={'/friendsfeed'}><PeopleOut className={iconClass + ' fill-neutral-500'}/></Link>
+      <Link to={'/chat'}><ChatOut className={iconClass + ' fill-neutral-500'}/></Link>
+    </>}
   </div>);
 };
 
